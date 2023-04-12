4e23839361cd89823625a3ca7eedadcd6610efe1
Rob Pilling <robpilling@gmail.com>
2023-04-12 19:57:35 +0100

    Factored out GetComponent<CharCtrler>

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : Singleton<PlayerInput> {
+    private CharacterController Character;
+
+    // Start is called before the first frame update
+    private void Start() {
+        Character = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     private void Update() {
@@ -14,8 +20,8 @@ public class PlayerInput : Singleton<PlayerInput> {
         var mousePos = Input.mousePosition;
         var mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
 
-        GetComponent<CharacterController>().MovementDirection = movementInput;
-        GetComponent<CharacterController>().FacingAt = mouseWorldPosition;
+        Character.MovementDirection = movementInput;
+        Character.FacingAt = mouseWorldPosition;
 
         if (Input.GetMouseButton(0)) {
             GetComponent<AttackController>().Shoot();
