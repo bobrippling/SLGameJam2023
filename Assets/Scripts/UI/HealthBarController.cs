ef9043315f5e0b8216780ca29d251da0a5384d8b
Andrew Jones <amjones@scottlogic.com>
2023-04-12 21:36:56 +0100

    Add health bar

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+    [SerializeField] private int myUniverse;
+
+    [SerializeField] private int maxHealth;
+
+    [SerializeField] private Image bar;
+
+    [SerializeField] private CharacterStats myStats;
+
+    // Start is called before the first frame update
+    private void Start() {
+        maxHealth = myStats.maxHealths[myUniverse];
+        myStats.OnHealthChange.AddListener(HandleHealthChange);
+    }
+
+    // Update is called once per frame
+    private void HandleHealthChange(int universe, int newHealth) {
+        if (myUniverse != universe) return;
+
+        bar.fillAmount = newHealth / maxHealth;
+    }
+}
