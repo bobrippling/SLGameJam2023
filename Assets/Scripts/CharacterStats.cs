cd897e3b26c8769531dd40fec7c2d6c249647bfb
Andrew Jones <amjones@scottlogic.com>
2023-04-12 20:55:36 +0100

    Add character health

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    [SerializeField] private List<int> maxHealths;
+    private List<int> currentHealths;
+
+    public void OnTakeDamage(int damage) {
+        var currentHealth = currentHealths[UniverseManager.Instance.currentUniverse];
+        currentHealths[UniverseManager.Instance.currentUniverse] -= Mathf.Max(currentHealth - damage, 0);
+    }
+}
