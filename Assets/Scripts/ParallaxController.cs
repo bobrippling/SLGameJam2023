b3590703397172a75b9bc4921c2778ed8d181ed0
Elliot Mason <emason@scottlogic.com>
2023-04-23 16:01:13 +0100

    Add Parallax Controller script

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    private GameObject playerObj = null;
+    private float distanceRatio = 0;
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         if (playerObj == null){
+             playerObj = GameObject.Find("Player");
+             distanceRatio = 500/(playerObj.transform.position.z - transform.position.z);
+        }
+        startPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(startPosition.x + (playerObj.transform.position.x / distanceRatio),
+                                     startPosition.y + (playerObj.transform.position.y / distanceRatio),
+                                     transform.position.z);
+    }
+}
